452f564cedbdad4d9ee3cacf2ec748649a07439d
abderahim9898 <meskabbi@gmail.com>
2025-11-19 11:14:41 +0100

    Updated Index.tsx

--- client/pages/Index.tsx
+++ client/pages/Index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { DataTable } from "@/components/DataTable";
 
 // Hardcoded Google Apps Script URL
-const GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxQVdsR_OFit9nEEzm4ksImuMLxMOk2cl1aVo27Ab4PuoTVaCiJn5UKNiw0DtP1BVB3VQ/exec";
+const GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxv7ixCbdWw4_iKpxeZN9GsQ_nQMNuy9gcsAGpSu7k3r2hydYS0A4UXWPcmraPxdXnlOw/exec";
 const SHEET_NAME = "ABSENCES";
 
 export default function Index() {
